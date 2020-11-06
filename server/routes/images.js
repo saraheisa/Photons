b697d97504b4fc04be4c0bf16a1a8a96344b2844
Sarah Eisa <prosarah10@gmail.com>
2020-11-07 01:33:24 +0200

    Feat: Create GET: /images routes
    This route used to get all images with some options
    page: Page number to retrieve (default = 1)
    per_page: Number of images to retrieve per page (default = 10)
    order_by: How images are ordered latest or oldest (default = latest)
    
    It returns a dummy response until DB is implemented

--- server/routes/images.js
+++ server/routes/images.js
@@ -24,29 +24,44 @@ imageRouter.post("/", upload.array('photo'), (req, res) => {
 imageRouter.delete("/", (req, res) => {
 
   const imageNames = req.body.images;
-  
+
   const notFoundImages = [];
   const successImages = [];
 
-  Promise.all(imageNames.map( (image) => {
+  Promise.all(imageNames.map((image) => {
     const imagePath = `${IMAGES_DIR_PATH}/${image}`;
     return unlinkAsync(imagePath)
-    .then(data => successImages.push(image))
-    .catch(err => notFoundImages.push(image));
+      .then(data => successImages.push(image))
+      .catch(err => notFoundImages.push(image));
   }))
-  .then(data => {
-    let code = notFoundImages.length > 1 ? 404 : 200;
-    return res.status(code).json({ success: successImages.length, 
+    .then(data => {
+      let code = notFoundImages.length > 1 ? 404 : 200;
+      return res.status(code).json({
+        success: successImages.length,
+        notFound: notFoundImages.length,
                                                   notFound: notFoundImages.length, 
+        notFound: notFoundImages.length,
+        successImages,
                                                   successImages, 
-                                                  notFoundImages });
-  })
-  .catch(err => console.log(err));
+        successImages,
+        notFoundImages
+      });
+    })
+    .catch(err => console.log(err));
 
 });
 
+imageRouter.get("/", (req, res) => {
+  const page = req.query.page || 1;
+  const per_page = req.query.per_page || 10;
+  const order_by = req.query.order_by || 'latest';
+  console.log(`${page} - ${per_page} - ${order_by}`);
+  // TODO: get images from DB
+  return res.status(200).json({ images: true });
+});
+
 imageRouter.get("/:name",  (req, res) => {
-  
+
   const imagePath = `${IMAGES_DIR_PATH}/${req.params.name}`;
 
   if (!fs.existsSync(imagePath) ) {

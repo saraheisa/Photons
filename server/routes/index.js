6269517cc9bb0c86c19d69ba035edf52c08a5afc
Sarah Eisa <prosarah10@gmail.com>
2020-11-17 17:31:31 +0200

    Feat: Delete DB endpoints after making sure that my P.S is working
        No one can destroy me NOW!!!

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,14 +7,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/initialize',  (req, res) => {
-  createTables()
-  .then (result => {
-    return res.status(200).json({ result });
-  })
-  .catch(err => {
-    return res.status(500).json({ err });
-  });
-});
-
 module.exports = router;
